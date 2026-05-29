1e5111eb1379a66585863e8abbf82500fa8d77e3
Olaoluwasubomi <i@olaoluwasubomi.com>
2026-05-28 21:14:01 -0400

    fix(ci): refresh nix hashes for Triibal

--- nix/tui.nix
+++ nix/tui.nix
@@ -4,7 +4,7 @@ let
   src = ../ui-tui;
   npmDeps = pkgs.fetchNpmDeps {
     inherit src;
-    hash = "sha256-F6/MzZOWc0zhW9mIfnaY+PrllPvJcsA/OdFdEM+NpLY=";
+    hash = "sha256-2U08tj8FFoSupsJNlDkz+7VmXNhVud6xumhiUQEHaQw=";
   };
 
   npm = triibalNpmLib.mkNpmPassthru { folder = "ui-tui"; attr = "tui"; pname = "triibal-tui"; };
--- nix/web.nix
+++ nix/web.nix
@@ -4,7 +4,7 @@ let
   src = ../web;
   npmDeps = pkgs.fetchNpmDeps {
     inherit src;
-    hash = "sha256-6qhGuifHVtCeep1SiQdCUxBMr7UGhYpdMTvXhrQu/zA=";
+    hash = "sha256-HGyF8Uu87b3/AakZDSPrtfGuo6McscDJFZbV2+1SOWA=";
   };
 
   npm = triibalNpmLib.mkNpmPassthru { folder = "web"; attr = "web"; pname = "triibal-web"; };
